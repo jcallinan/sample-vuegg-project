cbd56fdd16d51a39429f47aaf30bc7c1e813291a
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-02-12 21:56:17 -0500

    Checking in generated vue.js files also
    in addition to .gg in intial checkin.

---
+++ my-vuegg-project/src/pages/Home.vue
@@ -0,0 +1,158 @@
+<template>
+<div id="Syx979EfXU">
+  <div class="ry3Qc4GX8"></div>
+  <div class="HJA7qVf7I"></div>
+  <table class="SJe4cEfQU">
+    <tr class="SJxe4q4fQI">
+      <th class="H1ZxEc4zmU">head 1</th>
+    
+      <th class="BkfeEcEzm8">head 2</th>
+    
+      <th class="rkmgEq4z7U">head 3</th>
+    </tr>
+  
+    <tr class="rkNlN5EG7I">
+      <td class="SyHgVq4GX8">data 1.1</td>
+    
+      <td class="SyLgN9NMmU">data 2.1</td>
+    
+      <td class="BkweVqNfX8">data 3.1</td>
+    </tr>
+  
+    <tr class="rydlEq4fXL">
+      <td class="B1txE5EMQI">data 1.2</td>
+    
+      <td class="By5eVqVG78">data 2.2</td>
+    
+      <td class="S1jgV5VzQ8">data 3.2</td>
+    </tr>
+  
+    <tr class="rJ3l4cNzmU">
+      <td class="rkplN5EG7L">data 3.1</td>
+    
+      <td class="rkRxN54fX8">data 3.2</td>
+    
+      <td class="rJylxE5VfXU">data 3.3</td>
+    </tr>
+  </table>
+  <mdc-drawer class="HJEBvqNzXU" open toolbar-spacer persistent toggle-on="toggledrawer">
+    <mdc-drawer-list class="ryHrwcNMQI">
+      <mdc-drawer-item class="H1LrP5EzQU" start-icon="inbox">Inbox</mdc-drawer-item>
+    
+      <mdc-drawer-item class="BkvSD5NMXL" start-icon="send">Sent Mail</mdc-drawer-item>
+    
+      <mdc-drawer-item class="rJ_BwcEGXL" start-icon="drafts">Drafts</mdc-drawer-item>
+    </mdc-drawer-list>
+  </mdc-drawer>
+  <mdc-card class="H1HdvqNGm8">
+    <mdc-card-media class="SkIOwqEzQU" height="160" src="https://material-components-web.appspot.com/images/16-9.jpg"></mdc-card-media>
+  
+    <mdc-card-header class="SyvOv5Ef78" title="Title" subtitle="Subtitle"></mdc-card-header>
+  
+    <mdc-card-actions class="HyO_vcNfQ8">
+      <mdc-card-action-button class="r1KODq4zmI">Action 1</mdc-card-action-button>
+    
+      <mdc-card-action-button class="Sk9_w94fmL">Action 2</mdc-card-action-button>
+    </mdc-card-actions>
+  </mdc-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#Syx979EfXU {
+  --mdc-theme-primary: #673ab7;
+  --mdc-theme-secondary: #f44336;
+  --mdc-theme-background: #ffffff;
+  position: relative;
+  margin: auto;
+  background-color: #ffffff;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.ry3Qc4GX8 {
+  overflow: hidden;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-style: solid;
+  border-width: 1px;
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 0px;
+  left: 0px;
+}
+
+.HJA7qVf7I {
+  overflow: hidden;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-style: solid;
+  border-width: 1px;
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 0px;
+  left: 639px;
+}
+
+.SyHgVq4GX8 {
+  padding: 0 8px;
+}
+
+.SyLgN9NMmU {
+  padding: 0 8px;
+}
+
+.BkweVqNfX8 {
+  padding: 0 8px;
+}
+
+.rkNlN5EG7I {
+  background-color: rgba(0, 0, 0, 0.12);
+}
+
+.rkplN5EG7L {
+  padding: 0 8px;
+}
+
+.rkRxN54fX8 {
+  padding: 0 8px;
+}
+
+.rJylxE5VfXU {
+  padding: 0 8px;
+}
+
+.rJ3l4cNzmU {
+  background-color: rgba(0, 0, 0, 0.12);
+}
+
+.SJe4cEfQU {
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border: 1px solid #dddddd;
+  text-align: left;
+  position: absolute;
+  width: 550px;
+  height: 300px;
+  top: 0px;
+  left: 471px;
+}
+
+.H1HdvqNGm8 {
+  background-color: #fff;
+  position: absolute;
+  width: 300px;
+  height: 310px;
+  top: 277px;
+  left: 490px;
+}
+
+</style>
